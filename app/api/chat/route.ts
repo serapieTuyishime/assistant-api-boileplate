163281d743b91b60efeef96d17de7befcea0bbae
serapieTuyishime <tuyiserapie1@gmail.com>
2023-12-18 18:54:11 +0200

    refactor: remove unecessary pages

--- app/api/chat/route.ts
+++
@@ -1,9 +0,0 @@
-import { kv } from '@vercel/kv'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
-
-import { nanoid } from '@/lib/utils'
-import { Configuration, OpenAIApi } from 'openai-edge'
-
-export async function POST(req: Request) {
-  console.log('this is the request', req)
-}
