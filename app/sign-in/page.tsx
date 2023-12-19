b8c8e4a484393a0dce4d3c7747075fb5c258547e
serapieTuyishime <tuyiserapie1@gmail.com>
2023-12-19 09:32:25 +0200

    refactor: remove the sign in page

--- app/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
-
-export default async function SignInPage() {
-  return (
-    <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
-      <Link href="/">Go home</Link>
-    </div>
-  )
-}
