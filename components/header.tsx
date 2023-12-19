debf823b06ea26843234e7a256e7beb604cca903
serapieTuyishime <tuyiserapie1@gmail.com>
2023-12-19 10:59:17 +0200

    refactor: remove the sidebar

--- components/header.tsx
+++ components/header.tsx
@@ -9,14 +9,14 @@ export async function Header() {
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <div className="flex items-center">
-        <Sidebar>
+        {/* <Sidebar>
           <Suspense
             fallback={<div className="flex-1 overflow-auto" />}
           ></Suspense>
           <Button variant="link" asChild className="-ml-2">
             <ClearThread />
           </Button>
-        </Sidebar>
+        </Sidebar> */}
         <div className="flex items-center">
           <IconSeparator className="w-6 h-6 text-muted-foreground/50" />
 
